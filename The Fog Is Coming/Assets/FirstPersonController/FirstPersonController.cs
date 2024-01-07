54ab996723c751eb1baf84e7feea8de72e89a804
Danylo <danildzekelev@gmail.com>
2024-01-07 15:45:09 +0200

    Write player movement and camera cotnrol script.

--- The Fog Is Coming/Assets/FirstPersonController/FirstPersonController.cs
+++ The Fog Is Coming/Assets/FirstPersonController/FirstPersonController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class FirstPersonController : MonoBehaviour
 {
@@ -8,14 +9,61 @@ public class FirstPersonController : MonoBehaviour
         get;
         private set;
         } = true;
-    // Start is called before the first frame update
+    
+    [Header("Movement Parameters")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float gravity = 30.0f; // MB make ragdolls instead of this construction
+
+    [Header("Look Parameters")]
+    [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f; // Must be accessible from options
+    [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f; // Must be accessible from options
+    [SerializeField, Range(1, 180)] private float upLookLimit = 80.0f;
+    [SerializeField, Range(1, 180)] private float downLookLimit = 80.0f;
+
+    private Camera playerCamera;
+    private CharacterController characterController;
+
+    private Vector3 moveDirection;
+    private Vector2 currentInput;
+
+    private float rotationX = 0;
+
     void Start()
+    {
+        playerCamera = GetComponentInChildren<Camera>();
+        characterController = GetComponent<CharacterController>();        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (CanMove) 
+        {
+            HandleMovementInput();
+            HandleMouseLock();
+
+            AplyFinalMovements();
+        }
+    }
+
+    private void HandleMovementInput()
+    {
+        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), 
+        walkSpeed * Input.GetAxis("Horizontal"));
+
+        float moveDirectionY = moveDirection.y;
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x)
+        + (transform.TransformDirection(Vector3.right) * currentInput.y);
+        
+        moveDirection.y = moveDirectionY;
+
+    }
+    private void HandleMouseLock()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void AplyFinalMovements()
     {
         
     }
