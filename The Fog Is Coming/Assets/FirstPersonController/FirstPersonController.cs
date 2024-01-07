121c088746cf8a82386be9f11ee536911588f8db
Danylo <danildzekelev@gmail.com>
2024-01-07 15:28:44 +0200

    Write bool CanMove.

--- The Fog Is Coming/Assets/FirstPersonController/FirstPersonController.cs
+++ The Fog Is Coming/Assets/FirstPersonController/FirstPersonController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
+    public bool CanMove { 
+        get;
+        private set;
+        } = true;
     // Start is called before the first frame update
     void Start()
     {
