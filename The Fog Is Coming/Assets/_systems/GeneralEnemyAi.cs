ba04a1523ff54454db75101f3b454447816571a7
Danylo <danildzekelev@gmail.com>
2024-01-18 13:33:51 +0200

    Set player tag.

--- The Fog Is Coming/Assets/_systems/GeneralEnemyAi.cs
+++ The Fog Is Coming/Assets/_systems/GeneralEnemyAi.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GeneralEnemyAi : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public NavMeshAgent ai;
+    public List<Transform> destinations;
+    public Animator aiAnim;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
