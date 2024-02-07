0bdc39d3df3226ed4504050cf17fca59734ddb33
Danylo <danildzekelev@gmail.com>
2024-02-07 13:25:38 +0200

    Write anim and transform for door.

--- The Fog Is Coming/Assets/_scripts/DoorOpen.cs
+++ The Fog Is Coming/Assets/_scripts/DoorOpen.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Doors : Interaction
 {
     private bool isOpen = false;
     private bool canBeInteractedWith = false;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     public override void OnFocus()
     {
 
@@ -13,7 +20,14 @@ public class Doors : Interaction
 
     public override void OnInteract()
     {
-        throw new System.NotImplementedException();
+        if (canBeInteractedWith)
+        {
+            isOpen = !isOpen;
+
+            Vector3 doorTransformDi = transform.TransformDirection(Vector3.forward);
+
+            Vector3 playerTransformDir;
+        }
     }
 
     public override void OnLoseFocus()
